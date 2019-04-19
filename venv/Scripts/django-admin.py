22acceb9bb636960dc65bc52f275cd352a630250
i-yosuke <iyimiz102902@gmail.com>
2019-04-19 15:22:28 +0900

    My Django Girls app, first commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\yosuk\Desktop\10_開発\90_実装\djangoGirlsSite\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
